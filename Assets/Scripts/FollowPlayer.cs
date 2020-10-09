0bf67843907cecb574fb266799f5b219d3301a8a
Nguyen Vu Nguyen <39196799+nvnguyen-ki@users.noreply.github.com>
2020-10-08 22:21:11 -0400

    MainScene started.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+    void LateUpdate()
+    {
+        
+
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothedPos = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothSpeed);
+        transform.position = smoothedPos;
+
+        transform.rotation = target.rotation;
+
+       
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,48 @@ using UnityEngine;
 // inherit from MonoBehavioor
 public class PlayerController : MonoBehaviour
 {
+    float speed = 15.0f;
+    private float turnSpeed = 45f;
+    private float MouseInput;
+    private float VerticalInput;
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called before the first frame update
+
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // update the vehicle movement
 
-        // move foward : moving the z (60 units in a second)
-        // transform.Translate(0, 0, 1) is the same as the code below
-        transform.Translate(Vector3.forward * (Time.deltaTime * 20));
+        
+        // update the vehicle movement
+        MouseInput = Input.GetAxis("Mouse X");
+        VerticalInput = Input.GetAxis("Vertical");
+
+        // move foward : moving the z (60 units in a second) based on vertical input
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * VerticalInput));
+
+        // turn : rotate the car based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * MouseInput);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+    
+
+}
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
