75aa9d93cd9f6e41df2dc91c5aaad39188c4a69e
Nguyen Vu Nguyen <39196799+nvnguyen-ki@users.noreply.github.com>
2020-10-12 17:37:53 -0400

    poly char + health bar
    found free poly char and started new scene. Created a health bar.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -8,21 +8,11 @@ public class FollowPlayer : MonoBehaviour
     public Vector3 offset;
     private Vector3 velocity = Vector3.zero;
 
-
-    void Start()
-    {
-        offset = transform.position - target.position;
-    }
     void LateUpdate()
     {
-        
+       Vector3 desiredPos = target.position + offset;
+       Vector3 smoothedPos = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothSpeed);
+       transform.position = smoothedPos;
 
-        Vector3 desiredPos = target.position + offset;
-        Vector3 smoothedPos = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothSpeed);
-        transform.position = smoothedPos;
-
-        transform.rotation = target.rotation;
-
-       
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 // inherit from MonoBehavioor
 public class PlayerController : MonoBehaviour
 {
-    float speed = 15.0f;
+    float speed = 3.0f;
+    float runSpeed = 6.0f;
     private float turnSpeed = 45f;
     private float MouseInput;
     private float VerticalInput;
@@ -14,39 +16,87 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     // Start is called before the first frame update
-
+    Animator anim;
+    int isWalking;
+    int isRunning;
+    public float maxHealth = 100;
+    public float currentHealth;
+    public HealthBar healthBar;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
-
+        anim = GetComponent<Animator>();
+        isWalking = Animator.StringToHash("isWalking");
+        isRunning = Animator.StringToHash("isRunning");
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-
         
         // update the vehicle movement
         MouseInput = Input.GetAxis("Mouse X");
         VerticalInput = Input.GetAxis("Vertical");
+        bool runShift = Input.GetKey("left shift");
+        bool jump = Input.GetKey("space");
 
         // move foward : moving the z (60 units in a second) based on vertical input
-        transform.Translate(Vector3.forward * (Time.deltaTime * speed * VerticalInput));
-
+        
         // turn : rotate the car based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * MouseInput);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
+        if (VerticalInput > 0)
+        {
+            transform.Translate(Vector3.forward * (Time.deltaTime * speed * VerticalInput));
+            anim.SetBool(isWalking, true);
+            anim.SetFloat("Speed", 1.0f);
+            takeDamage(0.01f);
+        } 
+        else if (VerticalInput < 0)
+        {
+            transform.Translate(Vector3.forward * (Time.deltaTime * speed * VerticalInput));
+            anim.SetBool(isWalking, true);
+            anim.SetFloat("Speed", -1.0f);
+            
+        } else
+        {
+            anim.SetBool(isWalking, false);
         }
 
+        if (VerticalInput > 0 && runShift)
+        {
+            anim.SetBool(isRunning, true);
+            transform.Translate(Vector3.forward * (Time.deltaTime * runSpeed * VerticalInput));
+        } else
+        {
+            transform.Translate(Vector3.forward * (Time.deltaTime * speed * VerticalInput));
+            anim.SetBool(isRunning, false);
+        }
+
+
+        /*if (jump && isOnGround) {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            anim.SetBool(isJumping, true);
+        } else if (!jump)
+        {
+            anim.SetBool(isJumping, false);
+        }
+        */
     
 
 }
+    void takeDamage(float damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+    
     private void OnCollisionEnter(Collision collision)
     {
         isOnGround = true;
     }
+    
 }
